9d31a6d1396ee4f2552fa3cac393478f449d5bb4
kyorohiro <kyorohiro@gmail.com>
2019-01-16 07:11:18 +0900

    Hello Worldgit add -A

--- lib/main.dart
+++ lib/main.dart
@@ -8,46 +8,16 @@ class MyApp extends fl.StatelessWidget {
   fl.Widget build(fl.BuildContext context) {
     return new fl.MaterialApp(
         title: "Welcome to Fluttr",
-        home: MyHomePage("Welcome to Fluttr"));
+        home: MyHomePage());
   }
 }
 
-class MyHomePage extends fl.StatefulWidget {
-  final String title;
-  MyHomePage(this.title);
+class MyHomePage extends fl.StatelessWidget {
   @override
-  fl.State<fl.StatefulWidget> createState() {
-    return new MyHomePageState();
-  }
-}
-
-class MyHomePageState extends fl.State<MyHomePage> {
-  bool _visible = true;
-
   fl.Widget build(fl.BuildContext context) {
-    return fl.Scaffold(
-      appBar: fl.AppBar(title: fl.Text(widget.title),),
-      body: fl.Center(
-        child: fl.AnimatedOpacity(
-          opacity: _visible ? 1.0 : 0.0,
-          duration: Duration(milliseconds: 50),
-          child: fl.Container(
-            width: 200.0,
-            height: 200.0,
-            color: fl.Colors.green,
-          ),
-        ),
-      ),
-      floatingActionButton: fl.FloatingActionButton(
-        onPressed: (){
-          print(">>>>>> PRESSED");
-          setState(() {
-            _visible = !_visible;
-          });
-        },
-        tooltip: "Toggle Opacity",
-        child: fl.Icon(fl.Icons.flip),
-      ),
+    return new fl.Scaffold(
+      appBar: fl.AppBar(title: fl.Text("title"),),
+      body: fl.Text("Hello World")
     );
   }
 }
