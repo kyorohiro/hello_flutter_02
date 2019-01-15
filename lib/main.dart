5f8ff9ea36e67cf7514e555102cf5fa48f6e4536
kyorohiro <kyorohiro@gmail.com>
2019-01-16 04:00:07 +0900

    Step2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart' as fl;
+import 'package:english_words/english_words.dart' as en;
 
 void main() {
   fl.runApp(MyApp());
@@ -6,6 +7,7 @@ void main() {
 
 class MyApp extends fl.StatelessWidget {
   fl.Widget build(fl.BuildContext context) {
+    final wordPair = en.WordPair.random();
     fl.MaterialApp app =
       new fl.MaterialApp(
         title: "Welcome to Fluttr",
@@ -13,7 +15,7 @@ class MyApp extends fl.StatelessWidget {
           title: fl.Text("Welcome to Flutter"),
         ),
         body: fl.Center(
-          child: fl.Text("Hello World!!"),
+          child: fl.Text(wordPair.asPascalCase),
         ),)
       );
     return app;
