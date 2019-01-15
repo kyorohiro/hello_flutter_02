6a52e0a56ff08fd9beab78018638b58a0598bee7
kyorohiro <kyorohiro@gmail.com>
2019-01-16 04:52:53 +0900

    STEP4

--- lib/main.dart
+++ lib/main.dart
@@ -29,11 +29,46 @@ class RandomWords extends fl.StatefulWidget {
   fl.State<fl.StatefulWidget> createState() {
     return new RandomWorldsState();
   }
+
 }
 
 class RandomWorldsState extends fl.State<RandomWords> {
-  fl.Widget build(fl.BuildContext context) {
+  final _suggestions = <en.WordPair>[];
+  final _beggerFont = const fl.TextStyle(fontSize: 18.0);
+  /*fl.Widget build(fl.BuildContext context) {
     final wordPair = en.WordPair.random();
     return fl.Text(wordPair.asPascalCase);
+  }
+  */
+  @override
+  fl.Widget build(fl.BuildContext context) {
+    return fl.Scaffold(
+      appBar: fl.AppBar(
+        title: fl.Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  fl.Widget _buildSuggestions() {
+    return fl.ListView.builder(
+      padding: const fl.EdgeInsets.all(16.0),
+      itemBuilder: (fl.BuildContext context, int i) {
+        if(i.isOdd) {
+          return fl.Divider();
+        }
+
+        final index = i ~/2;
+        if(index >= _suggestions.length) {
+          _suggestions.addAll(en.generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  fl.Widget _buildRow(en.WordPair pair) {
+    return fl.ListTile(
+      title: new fl.Text(pair.asPascalCase, style: _beggerFont),
+    );
   }
 }
