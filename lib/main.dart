eb360546d2229fccfdbcf7e4ce227ec1a55747d4
kyorohiro <kyorohiro@gmail.com>
2019-01-16 06:09:38 +0900

    Hello Worldgit add -A

--- lib/main.dart
+++ lib/main.dart
@@ -11,60 +11,13 @@ class MyApp extends fl.StatelessWidget {
     fl.MaterialApp app =
       new fl.MaterialApp(
         title: "Welcome to Fluttr",
-        home: fl.Scaffold(appBar: fl.AppBar(
-          title: fl.Text("Welcome to Flutter"),
-        ),
-        body: fl.Center(
-          child:
-          //fl.Text(wordPair.asPascalCase),
-          RandomWords(),
-        ),)
+        home: fl.Scaffold (
+            appBar: fl.AppBar(
+                title: fl.Text("Welcome to Flutter")),
+            body: fl.Center(
+                child:fl.Text(wordPair.asPascalCase))
+        )
       );
     return app;
-  }
-}
-
-class RandomWords extends fl.StatefulWidget {
-  @override
-  fl.State<fl.StatefulWidget> createState() {
-    return new RandomWorldsState();
-  }
-
-}
-
-class RandomWorldsState extends fl.State<RandomWords> {
-  final _suggestions = <en.WordPair>[];
-  final _beggerFont = const fl.TextStyle(fontSize: 18.0);
-
-  @override
-  fl.Widget build(fl.BuildContext context) {
-    return fl.Scaffold(
-      appBar: fl.AppBar(
-        title: fl.Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-  fl.Widget _buildSuggestions() {
-    return fl.ListView.builder(
-      padding: const fl.EdgeInsets.all(16.0),
-      itemBuilder: (fl.BuildContext context, int i) {
-        if(i.isOdd) {
-          return fl.Divider();
-        }
-
-        final index = i ~/2;
-        if(index >= _suggestions.length) {
-          _suggestions.addAll(en.generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
-
-  fl.Widget _buildRow(en.WordPair pair) {
-    return fl.ListTile(
-      title: new fl.Text(pair.asPascalCase, style: _beggerFont),
-    );
   }
 }
